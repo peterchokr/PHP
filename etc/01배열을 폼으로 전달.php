8b0c9c2ab7d88291e2aa43544a003ae66f6d3455
Cho jeonghyun <peterchokr@gmail.com>
2023-01-03 17:03:11 +0900

    php

--- etc/01배열을 폼으로 전달.php
+++
@@ -1,8 +0,0 @@
-<?php
-$a = array("일", "이", "삼");
-$a_send = urlencode(serialize($a));
-?>
-<form action="a2.php" method="post">
-    <input type="hidden" name="arr" value="<?= $a_send?>">
-    <input type="submit" value="전달">
-</form>
